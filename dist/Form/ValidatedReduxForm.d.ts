8f8ec616cbe9975a88523be4aea91de6ea7ca101
AJ Cloutier <anthony.cloutier@tcengine.com>
2020-10-17 04:58:37 -0500

    updated version and rebuilt dist

--- dist/Form/ValidatedReduxForm.d.ts
+++ dist/Form/ValidatedReduxForm.d.ts
@@ -1,3 +1,22 @@
+import React from 'react';
+import { ConnectedProps } from 'react-redux';
+import { IValidatedFormProps } from './validatedFormModels';
+declare type OwnProps = Omit<IValidatedFormProps, 'onFormChanged' | 'onSubmit'>;
+declare const connector: any;
+declare type PropsFromRedux = ConnectedProps<typeof connector>;
+export declare class ValidatedReduxForm extends React.Component<OwnProps & PropsFromRedux> {
+    static defaultProps: {
+        customValidators: {};
+        initialFieldValues: {};
+        formErrorClass: string;
+    };
+    componentDidMount(): void;
+    formRef: React.RefObject<HTMLFormElement>;
+    onFormSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+    fieldChanged: (e: any) => void;
+    resetForm: () => void;
+    render(): JSX.Element;
+}
 declare const _default: any;
 export default _default;
 //# sourceMappingURL=ValidatedReduxForm.d.ts.map
--- dist/Form/shared.d.ts
+++ dist/Form/shared.d.ts
@@ -1,7 +1,12 @@
+import { ReactNode } from 'react';
+import { FormValues } from './validatedFormModels';
+import { ValidatedReduxForm } from './ValidatedReduxForm';
+import ValidatedForm from './ValidatedForm';
 export declare const isFieldValidatable: (el: HTMLFormElement) => boolean;
 export declare const getFieldsInForm: (form: HTMLFormElement | null) => HTMLFormElement[];
 export declare const getUpdatedFormValue: (el: HTMLFormElement, props: any, init?: boolean) => {
     value: any;
     error: string;
 };
+export declare function attachProps(this: ValidatedForm | ValidatedReduxForm, startingChildren: ReactNode, formFields: HTMLFormElement[], formValues: FormValues): any;
 //# sourceMappingURL=shared.d.ts.map
