8674a1f9f73203fbe08904bab2d42bdc076e0bc0
AJ Cloutier <anthony.cloutier@tcengine.com>
2021-05-07 12:25:51 -0500

    remove old comment

--- src/Demo/index.tsx
+++ src/Demo/index.tsx
@@ -3,11 +3,6 @@ import StandardForm from './StandardFormDemo';
 import ReduxForm from './ReduxFormDemo';
 import './demo.scss';
 
-// TODO: fix duplicate code -- use composition
-// add logic to store "type" along with name of field -- if there are multiples with the same name and they are different types, throw an error
-// (ex: multiple radios are expected to share a name, but not two different types of inputs)
-// This logic can be easily added to getFieldsInForm in shared.ts?
-
 export default () => (
   <div className="demo-container">
     <ReduxForm />
